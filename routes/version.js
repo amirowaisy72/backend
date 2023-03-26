39e819714e381dab4a92017b7f5479eb77a3fda7
amirowaisy72 <128407308+amirowaisy72@users.noreply.github.com>
2023-03-26 18:22:57 +0500

    added link variable

--- routes/version.js
+++ routes/version.js
@@ -4,7 +4,8 @@ const router = express.Router();
 // {Read/Fetch Operation}
 router.get("/", async (req, res) => {
     const version = 1
-    res.json(version);
+    const link = ""
+    res.json({link:link, version:version});
   });
 
 module.exports = router;
