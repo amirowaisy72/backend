d1b1fe929d7891e89474aee72c8f65d154666a57
amirowaisy72 <128407308+amirowaisy72@users.noreply.github.com>
2023-04-05 03:48:19 +0500

    Version 2 link

--- routes/version.js
+++ routes/version.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // {Read/Fetch Operation}
 router.get("/", async (req, res) => {
     const version = 2
-    const link = "https://drive.google.com/file/d/194d41QvbSi-YipfIr1cOfiumQC6hrwnZ/view?usp=sharing"
+    const link = "https://drive.google.com/file/d/1Yi2t2XDmh1i4toLHh6HAyCxzIYxy7ITN/view?usp=sharing"
     res.json({link:link, version:version});
   });
 
